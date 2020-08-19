f8f04d179b4840bad5ae7f6e79e1d05d0fc447c9
Shizuku-Asami <31030499+Shizuku-Asami@users.noreply.github.com>
2020-08-19 21:32:49 +0300

    Add files via upload

---
+++ code/pmatrix.js
@@ -0,0 +1,35 @@
+function pmatrix(row, column, symbol) {
+    let array = [];
+    if (symbol === '') {
+        for (let i = 0; i < row; i++) {
+            array[i] = [];
+            for (let j = 0; j < column; j++) {
+                if (j < column - 1) {
+                    array[i][j] = ` & `;
+                }
+                else {
+                    array[i][j] = ` \\\\ \n`;     
+                }
+            }
+            array[i] = array[i].join('');
+        }
+    let string = '\\begin{pmatrix} \n' + array.join('') + '\\end{pmatrix} \n';
+    return string;
+    }
+    for (let i = 0; i < row; i++) {
+        array[i] = [];
+        for (let j = 0; j < column; j++) {
+            if (j < column - 1) {
+                array[i][j] = `${symbol}_{${i+1},${j+1}} & `;
+            }
+            else {
+                array[i][j] = `${symbol}_{${i+1},${j+1}} \\\\ \n`;     
+            }
+        }
+        array[i] = array[i].join('');
+    }
+    let string = '\\begin{pmatrix} \n' + array.join('') + '\\end{pmatrix} \n';
+    return string;
+}
+
+console.log(pmatrix(8, 5, 'A'));
